feedad676d6059824eee6a05780447c03f783e71
acastillo122 <alycastillo@Alys-Air.lan>
2024-12-15 17:55:20 -0500

    does this work now

--- songs.js
+++ songs.js
@@ -1,209 +1,209 @@
 export const songs = [
     {
-        level: 1,
+        "level": 1,
         bpm: 50,
         name: "Open Arms",
         artist: "Journey",
         link: "https://www.youtube.com/watch?v=3ByIYof4mqo"
     },
     {
-        level: 1,
+        "level": 1,
         bpm: 55,
         name: "Somebody To Love",
         artist: "Queen",
         link: "https://www.youtube.com/watch?v=g-DF1Mzy_3g"
     },
     {
-        level: 1,
+        "level": 1,
         bpm: 60,
         name: "Simple Man",
         artist: "Lynyrd Skynyrd",
         link: "https://www.youtube.com/watch?v=8eNoms9wsGc"
     },
     {
-        level: 2,
+        "level": 2,
         bpm: 60,
         name: "Exhausted",
         artist: "Foo Fighters",
         link: "https://www.youtube.com/watch?v=Ttzjq29EgwU"
     },
     {
-        level: 2,
+        "level": 2,
         bpm: 65,
         name: "Something In The Way",
         artist: "Nirvana",
         link: "https://www.youtube.com/watch?v=4VxdufqB9zg"
     },
     {
-        level: 2,
+        "level": 2,
         bpm: 70,
         name: "The Unforgiven",
         artist: "Metallica",
         link: "https://www.youtube.com/watch?v=domjqjQ_WRI"
     },
     {
-        level: 3,
+        "level": 3,
         bpm: 70,
         name: "I Could Have Lied",
         artist: "Red Hot Chili Peppers",
         link: "https://www.youtube.com/watch?v=4upIYEHHM2o"
     },
     {
-        level: 3,
+        "level": 3,
         bpm: 75,
         name: "Hotel California",
         artist: "Eagles",
         link: "https://www.youtube.com/watch?v=BciS5krYL80"
     },
     {
-        level: 3,
+        "level": 3,
         bpm: 80,
         name: "Undone - The Sweater Song",
         artist: "Weezer",
         link: "https://www.youtube.com/watch?v=mLawKw3Qgig"
     },
     {
-        level: 4,
+        "level": 4,
         bpm: 80,
         name: "Come Out and Play",
         artist: "The Offspring",
         link: "https://www.youtube.com/watch?v=GHUql3OC_uU"
     },
     {
-        level: 4,
+        "level": 4,
         bpm: 85,
         name: "Interstate Love Song",
         artist: "Stone Temple Pilots",
         link: "https://www.youtube.com/watch?v=UjjyC8lmoQs"
     },
     {
-        level: 4,
+        "level": 4,
         bpm: 90,
         name: "Santeria",
         artist: "Sublime",
         link: "https://www.youtube.com/watch?v=upoEYQ7dcEk"
     },
     {
-        level: 5,
+        "level": 5,
         bpm: 90,
         name: "Drive",
         artist: "Incubus",
         link: "https://www.youtube.com/watch?v=fgT9zGkiLig"
     },
     {
-        level: 5,
+        "level": 5,
         bpm: 95,
         name: "Contact",
         artist: "The Police",
         link: "https://www.youtube.com/watch?v=1UoY19flAj0"
     },
     {
-        level: 5,
+        "level": 5,
         bpm: 100,
         name: "Re-Hash",
         artist: "Gorillaz",
         link: "https://www.youtube.com/watch?v=Tv1SYqLllKI"
     },
     {
-        level: 6,
+        "level": 6,
         bpm: 100,
         name: "Man in the Mirror",
         artist: "Michael Jackson",
         link: "https://www.youtube.com/watch?v=Z9NYDgbKsBE"
     },
     {
-        level: 6,
+        "level": 6,
         bpm: 105,
         name: "Black Hole Sun",
         artist: "Soundgarden",
         link: "https://www.youtube.com/watch?v=3mbBbFH9fAg"
     },
     {
-        level: 6, 
+        "level": 6, 
         bpm: 110,
         name: "Bobo On The Corner",
         artist: "Beastie Boys",
         link: "https://www.youtube.com/watch?v=e3CX8MUhuZY"
     },
     {
-        level: 7,
+        "level": 7,
         bpm: 110,
         name: "Don't Think Twice, It's All Right",
         artist: "Bob Dylan",
         link: "https://www.youtube.com/watch?v=1iHhWh9FtsQ"
     },
     {
-        level: 7,
+        "level": 7,
         bpm: 115,
         name: "Everything She Wants",
         artist: "Wham!",
         link: "https://www.youtube.com/watch?v=vDYw__at664"
     },
     {
-        level: 7,
+        "level": 7,
         bpm: 120,
         name: "All She's Got",
         artist: "Sum 41",
         link: "https://www.youtube.com/watch?v=UxtMzaiqjew"
     },
     {
-        level: 8,
+        "level": 8,
         bpm: 120,
         name: "It's My Life",
         artist: "Bon Jovi",
         link: "https://www.youtube.com/watch?v=bY3vXr7fm8k"
     },
     {
-        level: 8,
+        "level": 8,
         bpm: 125,
         name: "I'm so Afraid",
         artist: "Fleetwood Mac",
         link: "https://www.youtube.com/watch?v=ZN3MneMAX9s"
     },
     {
-        level: 8,
+        "level": 8,
         bpm: 130,
         name: "Big Me",
         artist: "Foo Fighters",
         link: "https://www.youtube.com/watch?v=pLdJQFTnZfA"
     },
     {
-        level: 9,
+        "level": 9,
         bpm: 130,
         name: "Venice Queen",
         artist: "Red Hot Chili Peppers",
         link: "https://www.youtube.com/watch?v=3s86rJvMIS0"
     },
     {
-        level: 9,
+        "level": 9,
         bpm: 135,
         name: "Dancing in the Moonlight",
         artist: "King Harvest",
         link: "https://www.youtube.com/watch?v=g5JqPxmYhlo"
     },
     {
-        level: 9,
+        "level": 9,
         bpm: 140,
         name: "Lovesong",
         artist: "The Cure",
         link: "https://www.youtube.com/watch?v=cI0QizfB7qo"
     },
     {
-        level: 10,
+        "level": 10,
         bpm: 140,
         name: "Know You Now",
         artist: "Amy Winehouse",
         link: "https://www.youtube.com/watch?v=Frfb3cfmjt4"
     },
     {
-        level: 10,
+        "level": 10,
         bpm: 145,
         name: "Walking On The Moon",
         artist: "The Police",
         link: "https://www.youtube.com/watch?v=kmJce02JHWM"
     },
     {
-        level: 10,
+        "level": 10,
         bpm: 150,
         name: "She Loves You",
         artist: "The Beatles",
