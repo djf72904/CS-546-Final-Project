fb2522f6d43a095d2fc41758d45a23765bae01ef
JackTPatterson <Jtpatt03@gmail.com>
2024-12-16 18:57:28 -0500

    fixed comment dialog

--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,7 @@ import {createSecretToken, hashPassword} from "../scripts/handlers/authHandlers.
 import {User} from "../scripts/db/config/schema.js";
 import {authHandlers} from "../middleware.js";
 let router = express.Router();
+import { v4 as uuidv4 } from 'uuid';
 
 /* GET users listing. */
 router.get('/', authHandlers, function(req, res, next) {
@@ -45,9 +46,8 @@ router.post('/', authHandlers,  async (req, res) => {
   
   const encryptedPassword = hashPassword(password);
 
-  const newUser = new User({ password: encryptedPassword, email });
+  const newUser = new User({ _id: uuidv4().toString(), password: encryptedPassword, email });
   await newUser.save();
-
 
   const token = createSecretToken(newUser._id);
 
