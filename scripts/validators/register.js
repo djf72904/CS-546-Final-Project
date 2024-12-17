4be3a1f60d4de566fe593f2fdabd5a738e91b83f
Zeynep Alta <Zeynep03a@gmail.com>
2024-12-16 22:17:14 -0500

    fixed register username validation

--- scripts/validators/register.js
+++ scripts/validators/register.js
@@ -24,7 +24,7 @@ export const validateUserName = (userName) => {
         throw new Error('Error: Input display name must be a string.');
     if (/\s/.test(userName)) 
         throw new Error('Error: Input display name cannot contain spaces.');
-    if(!uuidValidate(user_id)) 
+    if(!uuidValidate(userName)) 
         throw new Error("editDisplayName Error: user_id must be valid uuid");
     // if (!/\d/.test(password)) 
     //     throw new Error('Error: Input password must contain at least one number.');
